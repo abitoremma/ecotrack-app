179d285f599c37849c810ae186661b864bce128b
emmanuel <abitoremma@gmail.com>
2026-06-19 15:56:54 +0200

    Second commit: correction du app.py

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 # app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "EcoTrack fonctionne"
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=80
+    )
+
 print("=== EcoTrack Application ===")
 print("Tentative de connexion à la base de données RDS...")
 # Plus tard, on ajoutera le vrai code de connexion ici
