1b4075c66fa0f25f100ac41d865f24c023d0fc46
emmanuel <abitoremma@gmail.com>
2026-06-19 16:18:17 +0200

    Troisième commit: correction du app.py

--- app.py
+++ app.py
@@ -13,7 +13,4 @@ if __name__ == "__main__":
         port=80
     )
 
-print("=== EcoTrack Application ===")
-print("Tentative de connexion à la base de données RDS...")
-# Plus tard, on ajoutera le vrai code de connexion ici
-print("Connexion réussie ! L'application EcoTrack est en ligne.")
+
