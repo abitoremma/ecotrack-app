4bd38748117ead4ef946c4257edc48821050d1d0
emmanuel <abitoremma@gmail.com>
2026-06-19 14:39:52 +0200

    Premier commit: création de app.py et du gitignore

---
+++ app.py
@@ -0,0 +1,5 @@
+# app.py
+print("=== EcoTrack Application ===")
+print("Tentative de connexion à la base de données RDS...")
+# Plus tard, on ajoutera le vrai code de connexion ici
+print("Connexion réussie ! L'application EcoTrack est en ligne.")
